02072b18f500cedcf1622812459999b6d26ab381
Trifanov Sergey <trifanov.sergeyy@rambler.ru>
2023-10-09 22:03:04 +0300

    Решил

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,11 @@
-function makeFriendsList(friends) {
-  // ваш код...
+const makeFriendsList = list => {
+  const ul = document.createElement('ul');
+  
+  return list.reduce((ul, { firstName, lastName }) => {
+    const li = document.createElement('li');
+    li.textContent = `${firstName} ${lastName}`;
+    ul.appendChild(li);
+    
+    return ul;
+  }, ul);
 }
