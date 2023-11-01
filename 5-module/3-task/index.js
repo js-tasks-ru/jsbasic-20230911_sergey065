21ea0bc18016eb7a4226862c621676d1fdfae984
Trifanov Sergey <trifanov.sergeyy@rambler.ru>
2023-11-01 21:18:53 +0300

    Немного изменил решение. Теперь кнопки появляются и исчезают когда нужно.

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -13,22 +13,32 @@ function initCarousel() {
   prev.style.display = 'none';
 
   next.addEventListener('click', () => {
+    
+    step++
 
-    prev.style.display = '';
-
-    carousel.style.transform = `translateX(-${step += offsetWidth}px)`;
-
-    if (step >= offsetWidth * 3) {
+    carousel.style.transform = `translateX(${-step * offsetWidth}px)`;
+  
+    if (step >= 3) {
       next.style.display = 'none';
+    }
+
+    if (step > 0) {
+      prev.style.display = '';
     }
   })
 
   prev.addEventListener('click', () => {
 
-  carousel.style.transform = `translateX(-${step -= offsetWidth}px)`;
+  step--;
+
+  carousel.style.transform = `translateX(${-step * offsetWidth}px)`;
 
   if (step == 0) {
     prev.style.display = 'none';
+  }
+
+  if(step <= 3) {
+    next.style.display = '';
   }
 })
 }
